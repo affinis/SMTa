0ef29cfe0e2e9b862d47f89fd532770eaf90bc71
affinis <affinis@sjtu.edu.cn>
2022-10-24 20:07:08 +0800

    modified run_SMTa_test.sh

--- run_SMTa_test.sh
+++ run_SMTa_test.sh
@@ -18,24 +18,19 @@ sed -i -e "s|^A=.*|A=tinytest|g" -e "s|^A_image=.*|A_image=$SPR_PATH/external/sp
 
 if [ "`grep -o -m 1 'spaceranger-[0-9]' <<< $SPR_PATH`" = "spaceranger-1" ]
 then
-	echo "Using spaceranger 1.0.0"
-	sed -i "s|^REF=.*|REF=$SPR_PATH/external/spaceranger_tiny_ref/1.0.0|g" $SC_PATH/samples.info
-elif [ "`grep -o -m 1 '[0-9]\.[0-9]\.[0-9]' <<< $SPR_PATH`" = "2.0.0" ]
-then
-	echo "Using spaceranger 2.0.0"
-	sed -i "s|^REF=.*|REF=$SPR_PATH/external/spaceranger_tiny_ref|g" $SC_PATH/samples.info
 	echo "Using spaceranger 1.0.0 ref"
-	sed -i "s|^Reference=.*|Reference=$SPR_PATH/external/spaceranger_tiny_ref/1.0.0|g" samples.info
+	sed -i "s|^REF=.*|REF=$SPR_PATH/external/spaceranger_tiny_ref/1.0.0|g" $SC_PATH/samples.info
+
 elif [ "`grep -o -m 1 'spaceranger-[0-9]' <<< $SPR_PATH`" = "spaceranger-2" ]
 then
 	echo "Using spaceranger 2.0.0 ref"
-	sed -i "s|^Reference=.*|Reference=$SPR_PATH/external/spaceranger_tiny_ref|g" samples.info
+	sed -i "s|^Reference=.*|Reference=$SPR_PATH/external/spaceranger_tiny_ref|g" $SC_PATH/samples.info
 else
 	echo "tiny reference of spaceranger not found, you may provide an incorrect spaceranger path while installing"
 fi
 
 bash src/main.sh $SC_PATH/samples.info $SC_PATH/src/configs.conf
 
-sed -i "s|^SAMPLE_PATH=.*|SAMPLE_PATH=\"\"|g" samples.info
-sed -i -e "s|^A=.*|A=\"\"|g" -e "s|^A_image=|A_image=\"\"|g" samples.info
-sed -i "s|^REF=.*|REF=\"\"|g" samples.info
+sed -i "s|^SAMPLE_PATH=.*|SAMPLE_PATH=\"\"|g" $SC_PATH/samples.info
+sed -i -e "s|^A=.*|A=\"\"|g" -e "s|^A_image=.*|A_image=\"\"|g" $SC_PATH/samples.info
+sed -i "s|^REF=.*|REF=\"\"|g" $SC_PATH/samples.info
